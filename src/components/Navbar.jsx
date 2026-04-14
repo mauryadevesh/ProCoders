2a6b470783173d0c20657cdd98537c9be748560e
Aadii-stack <aaditya.r.mirashi25@slrtce.in>
2026-04-14 18:38:45 +0530

    dleted some files

--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import { styles } from "../styles/styles";
-
-export default function Navbar() {
-  return (
-    <nav style={styles.nav}>
-      <div style={styles.logo}>ProCoder</div>
-    </nav>
-  );
-}
